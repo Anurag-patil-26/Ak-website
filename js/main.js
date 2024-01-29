1b8e9e9bd70eb0f224236ce9532dced9720ee427
Anurag patil <113062557+Anurag-patil-26@users.noreply.github.com>
2024-01-29 19:03:27 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,13 +9,13 @@ $(document).ready(function(){
         $('.fa-bars').removeClass('fa-times');
         $('.navbar').removeClass('nav-toggle');
 
-        if($(window).scrollTop()>15)
+        if($(window).scrollTop()<35)
         {
-            $('.header').css({'background':'','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4)'});
+            $('.header').css({'background':'#ffff','box-shadow':'0 .2rem .5rem rgba(0,0,0,.4)'});
         }
         else
         {
-            $('.header').css({'background':'#d4d4d4','box-shadow':'none'});
+            $('.header').css({'background':'#fffff','box-shadow':'none'});
         }
     });
 
